ace8979944adf490c8730de1b15840932ad2ee39
Julian <obesepoptarts@obesepoptarts.ENG_1st_WLAN_Data.tamuk.edu>
2026-02-13 11:24:21 -0600

    Camera preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MainApp());
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const InteriorDesignARApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class InteriorDesignARApp extends StatelessWidget {
+  const InteriorDesignARApp({super.key});
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  CameraController? _controller;
+  bool _isCameraInitialized = false;
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    _controller = CameraController(
+      cameras.first,
+      ResolutionPreset.medium,
+    );
+    await _controller!.initialize();
+    setState(() {
+      _isCameraInitialized = true;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Interior Design AR"),
       ),
+      body: _isCameraInitialized
+          ? CameraPreview(_controller!)
+          : const Center(child: CircularProgressIndicator()),
     );
   }
 }
